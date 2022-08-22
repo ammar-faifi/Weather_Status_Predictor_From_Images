0e9327ace52fe96faafc8d11f11a16a390f5106e
ammar-faifi <a@ammarf.com>
2022-08-22 09:42:43 +0300

    add dash app

---
+++ app.py
@@ -0,0 +1,26 @@
+from dash import Dash, dcc, html, Input, Output
+import os
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
